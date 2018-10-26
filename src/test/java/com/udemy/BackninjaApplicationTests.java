c87eced15bab3dceb7852d30922384a16fcad930
JOSE MARCO HILARIO LUNA GAMBOA <jmhluna@scotiabank.com.mx>
2018-10-26 09:41:48 -0500

    add unit test

--- src/test/java/com/udemy/BackninjaApplicationTests.java
+++ src/test/java/com/udemy/BackninjaApplicationTests.java
@@ -1,16 +1,36 @@
 package com.udemy;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.udemy.model.CourseModel;
 
 
 public class BackninjaApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		
+		assertEquals(true, true);
+	}
+	
+	@Test
+	public void testCourseModel()
+	{
+		int id = 3;
+		String name = "demo";
+		String description = "description";
+		BigDecimal price = BigDecimal.valueOf(23);
+		int hours = 26;
+		CourseModel model = new CourseModel(id, name ,description, price, hours);
+		assertEquals(id, model.getId());
+		assertEquals(name, model.getName());
+		assertEquals(description, model.getDescription());
+		assertEquals(price, model.getPrice());
+		assertEquals(hours, model.getHours());
 	}
 
 }
